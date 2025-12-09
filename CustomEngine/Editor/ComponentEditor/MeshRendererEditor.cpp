7406397b84a949c929f494fba35eaee8681bd27b
paulzik <paulzik-93@hotmail.com>
2025-12-09 11:36:00 +0100

    Added Albedo texture as well in the component inspector

--- CustomEngine/Editor/ComponentEditor/MeshRendererEditor.cpp
+++ CustomEngine/Editor/ComponentEditor/MeshRendererEditor.cpp
@@ -15,6 +15,22 @@ void MeshRendererEditor::Draw(Component* component) {
     std::string fragmentShaderName = material->shader->GetFragmentShader()->name;
     ImGui::LabelText("FragmentShader", fragmentShaderName.c_str());
 
+    if (material->albedo) {
+        GLuint my_tex_id = material->albedo->ID;
+
+        ImVec2 size(32, 32);
+        ImVec2 uv0(0, 0);
+        ImVec2 uv1(1, 1);
+        ImVec4 bg_col(0, 0, 0, 1);
+        ImVec4 tint_col(1, 1, 1, 1);
+
+        ImGui::Text("Albedo");
+        ImGui::SameLine();
+
+        ImGui::ImageButton("##AlbedoBtn", (void*)(intptr_t)my_tex_id, size, uv0, uv1, bg_col, tint_col);
+    }
+
+
     float metallic = material->metallic;
     if (ImGui::SliderFloat("Metallic", &metallic, 0.0f, 1.0f)) {
         material->metallic = metallic;
